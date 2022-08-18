4aa5b716282625ea3c9f40be2811331071ddf9e2
alex <alexisjanssens@hotmail.fr>
2022-08-18 16:16:22 +0200

    first commit

--- front/js/product.js
+++ front/js/product.js
@@ -1,28 +1,63 @@
+// récupération de l'url de la page //
 const url = new URL(document.location.href);
+// récupération de la partie "id" de l'url //
 const idProduct = url.searchParams.get("id");
-console.log(idProduct);
-const urlID = "http://localhost:3000/api/products/" + idProduct;
+// requete de type "get" pour demander à l'API un produit en particulier ( correspondant à l'id présent dans l'url de la page) //
 fetch("http://localhost:3000/api/products/" + idProduct)
+// récuperation de la promise au format JSON //
     .then(function(res){
         if (res.ok) {
             return res.json();
         }
     })
+// affichage du JSON dans la console //
     .then(function(caractéristiques){
         console.log(caractéristiques);
+// appelle de la fonction pour afficher les caractéristiques du produit //
         affichageCaractéristiques(caractéristiques)
     })
-
+// déclaration de la fonction pour afficher les caractéristiques du produit//
 function affichageCaractéristiques(value) {
-    document.getElementsByClassName('item__img').innerHTML = `<img src="value.imageUrl" alt="value.altTxt">`;
+    document.querySelector('article div.item__img').innerHTML = `<img src="${value.imageUrl}" alt="${value.altTxt}">`;
     document.getElementById('description').innerText = value.description;
     document.getElementById('title').innerText = value.name;
     document.getElementById('price').innerText = value.price;
+// création d'une boucle pour afficher les couleurs du tableau de couleurs//
     for (let couleur of value.colors) {
-        document.getElementById('colors').innerHTML += `<option value="${couleur}">${couleur}</option>`
-    }
-}   
+        document.getElementById('colors').innerHTML += 
+        `<option value="${couleur}">${couleur}</option>`;
+}};
+// création de l'objet choixProduit //
+let choixProduit = {};
+// ajout de la clef "_id" à l'objet "choixProduit" //
+choixProduit._id = idProduct;
+//définitions des variables
+let choixCouleur = document.getElementById('colors');
+let couleurProduit ;
+let choixQuantité = document.getElementById('quantity');
+let quantitéProduit ;
+// création d'une fonction d'écoute pour récupérer la valeur de la couleur choisie
+choixCouleur.addEventListener("change", function(choix) {
+    couleurProduit = choix.target.value;
+    choixProduit.couleur = couleurProduit;
+    console.log(choixProduit)
+});
+// création d'une fonction d'écoute pour récupérer la valueur de la quantité choisie
+choixQuantité.addEventListener('change', function(choix) {
+    quantitéProduit = choix.target.value;
+    choixProduit.quantité = quantitéProduit;
+    console.log(choixProduit)
+});
 
+let panier = [];
 
+let validationPanier = document.getElementById('addToCart')
+
+validationPanier.addEventListener("click",function(ajout){
+    panier.push(choixProduit);
+    console.log(panier);
+})
+
+console.log("tout va bien");
 
     
--- front/js/script.js
+++ front/js/script.js
@@ -1,21 +1,26 @@
+// requête de type "get" pour récupérer l'ensemble des produits sous forme de tableau //
 fetch("http://localhost:3000/api/products/")
+// récupération de la promise au format JSON
   .then(function(res) {
     if (res.ok) {
       return res.json();
     }
   })
+// récupération dans la console du JSON dans la console sous forme de tableau
   .then(function(Canapés) {
     console.table(Canapés);
-    
+//appelle de la fonction pour l'affichage des produits sur la page d'accueil
     affichageCanapés(Canapés);
-    
   })
+//affichage d'une erreure en cas de problèmes
   .catch(function(err) {
-    // Une erreur est survenue
+    console.log("Une erreure est survenue..")
   });
 
+//déclaration de la fonction pour afficher les produits sur la page d'accueil
 function affichageCanapés(Canapés) {
     let créationCartes = document.getElementById('items');
+// création d'une boucle qui créé l'HTML pour chaque "kanap" du tableau "Canapés"
     for (let kanap of Canapés) {
         créationCartes.innerHTML += `
           <a href="./product.html?id=${kanap._id}">
