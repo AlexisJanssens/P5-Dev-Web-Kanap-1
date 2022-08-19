349c8f9271d7e6d5601b656bef360fb7f5f62af3
alex <alexisjanssens@hotmail.fr>
2022-08-19 23:47:51 +0200

    mise au propre commentaire + syntaxe

--- front/js/product.js
+++ front/js/product.js
@@ -1,37 +1,37 @@
-// récupération de l'url de la page //
+// récupération de l'url de la page 
 const url = new URL(document.location.href);
-// récupération de la partie "id" de l'url //
+// récupération de la partie "id" de l'url 
 const idProduct = url.searchParams.get("id");
-// requete de type "get" pour demander à l'API un produit en particulier ( correspondant à l'id présent dans l'url de la page) //
+// requete de type "get" pour demander à l'API un produit en particulier ( correspondant à l'id présent dans l'url de la page) 
 fetch("http://localhost:3000/api/products/" + idProduct)
-// récuperation de la promise au format JSON //
+// récuperation de la promise au format JSON 
     .then(function(res){
         if (res.ok) {
             return res.json();
         }
     })
-// affichage du JSON dans la console //
+// affichage du JSON dans la console 
     .then(function(caractéristiques){
         console.log(caractéristiques);
-// appelle de la fonction pour afficher les caractéristiques du produit //
+// appelle de la fonction pour afficher les caractéristiques du produit dans l'HTML 
         affichageCaractéristiques(caractéristiques)
     })
-// déclaration de la fonction pour afficher les caractéristiques du produit//
+// déclaration de la fonction pour afficher les caractéristiques du produit dans l'HTML
 function affichageCaractéristiques(value) {
     document.querySelector('article div.item__img').innerHTML = `<img src="${value.imageUrl}" alt="${value.altTxt}">`;
     document.getElementById('description').innerText = value.description;
     document.getElementById('title').innerText = value.name;
     document.getElementById('price').innerText = value.price;
-// création d'une boucle pour afficher les couleurs du tableau de couleurs//
+// création d'une boucle pour afficher les couleurs du tableau de couleurs
     for (let couleur of value.colors) {
         document.getElementById('colors').innerHTML += 
         `<option value="${couleur}">${couleur}</option>`;
 }};
-// création de l'objet choixProduit //
+// création de l'objet choixProduit 
 const choixProduit = {};
-// ajout de la clef "_id" à l'objet "choixProduit" //
+// ajout de la clef "_id" à l'objet "choixProduit" 
 choixProduit._id = idProduct;
-//définitions des variables
+// définitions des variables
 let choixCouleur = document.getElementById('colors');
 let couleurProduit ;
 let choixQuantité = document.getElementById('quantity');
@@ -51,7 +51,7 @@ choixQuantité.addEventListener('change', function(choix) {
 // definition des variables
 let validationPanier = document.getElementById('addToCart')
 // création d'une fonction d'écoute qui envoie l'objet "choixProduit" dans l'array panier si les conditions sont remplies
-validationPanier.addEventListener("click",function(ajout){
+validationPanier.addEventListener("click",function(){
     if (
         choixProduit.quantité < 1 ||
         choixProduit.quantité > 100 ||
@@ -63,12 +63,12 @@ validationPanier.addEventListener("click",function(ajout){
     } else {
     ajoutPanier(choixProduit)
 }})
-// fonction qui envoie le contenu du panier dans le LocalStorage
-function savePanier(panier) {
+// fonction qui envoie le contenu du panier dans le LocalStorage 
+function sauverPanier(panier) {
     localStorage.setItem("Panier", JSON.stringify(panier))
 }
 // fonction qui récupère le contenu du LocalStorage et qui crée un array en cas de panier null
-function getPanier() {
+function recupPanier() {
     let panier = localStorage.getItem("Panier");
     if (panier == null) {
         return [];
@@ -76,22 +76,31 @@ function getPanier() {
         return JSON.parse(panier)
     }
 }
+
 // définition de la fonction qui ajoute un produit dans le panier
 function ajoutPanier(produit) {
     // on récupère le panier du storage pour savoir si il est vide
-    let panier = getPanier();
-    // on compare les id et couleurs du produit ajouté avec le panier
-    let foundProductId = panier.find(p => p._id == produit._id);
-    let foundProductColor = panier.find(c => c.couleur == produit.couleur);
-    // si le même est déja présent, on auguemente juste la quantité 
-    if ((foundProductId != undefined) && (foundProductColor != undefined)) {
-        foundProductId.quantité = (foundProductId.quantité + produit.quantité);
+    let panier = recupPanier();
+    // on compare l'id du produit ajouté avec le panier
+    function verifId(produitPanier){
+        return produitPanier._id == produit._id
+     };
+    let produitTrouvéParId = panier.find(verifId);
+    // on compare la couleur du produit ajouté avec le panier
+    function verifCouleur(produitPanier){
+        return produitPanier.couleur == produit.couleur
+    };
+    let produitTrouvéParCouleur = panier.find(verifCouleur);
+    // si le même produit est déja présent, on auguemente juste la quantité 
+    if ((produitTrouvéParId != undefined) && (produitTrouvéParCouleur != undefined)) {
+        alert("Attention ! Vous avez déja au moins un exemplaire de ce produit dans le panier")
+        produitTrouvéParId.quantité = (produitTrouvéParId.quantité + produit.quantité);
     // sinon on l'ajoute au panier
     } else {
         panier.push(produit);
     }
     // on renvoi le tout dans le localStorage
-    savePanier(panier);
+    sauverPanier(panier);
 }
 console.log("tout va bien");
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-// requête de type "get" pour récupérer l'ensemble des produits sous forme de tableau //
+// requête de type "get" pour récupérer l'ensemble des produits sous forme de tableau 
 fetch("http://localhost:3000/api/products/")
 // récupération de la promise au format JSON
   .then(function(res) {
@@ -9,15 +9,15 @@ fetch("http://localhost:3000/api/products/")
 // récupération dans la console du JSON dans la console sous forme de tableau
   .then(function(Canapés) {
     console.table(Canapés);
-//appelle de la fonction pour l'affichage des produits sur la page d'accueil
+// appelle de la fonction pour l'affichage des produits sur la page d'accueil
     affichageCanapés(Canapés);
   })
-//affichage d'une erreure en cas de problèmes
+// affichage d'une erreure en cas de problèmes
   .catch(function(err) {
     console.log("Une erreure est survenue..")
   });
 
-//déclaration de la fonction pour afficher les produits sur la page d'accueil
+// déclaration de la fonction pour afficher les produits sur la page d'accueil
 function affichageCanapés(Canapés) {
     let créationCartes = document.getElementById('items');
 // création d'une boucle qui créé l'HTML pour chaque "kanap" du tableau "Canapés"
